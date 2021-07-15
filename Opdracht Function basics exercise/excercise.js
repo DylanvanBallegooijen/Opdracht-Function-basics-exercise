049caef566af78dec29d579ec435ff7581e0278a
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-15 16:23:08 +0200

    Add files via upload

---
+++ Opdracht Function basics exercise/excercise.js
@@ -0,0 +1,13 @@
+const dPizza = function(){
+    console.log('Making the dough')
+    console.log('Mix water and meel')
+    console.log('Kneed alles')
+    console.log('Laat rijzen')
+    console.log('Maak er een ronde vorm van')
+    console.log('Doe er tomatensaus op')
+    console.log('Doe er kaas en ander toppings op')
+    console.log('Oven voorverwarmen 250 graden')
+    console.log('paar minuten in de oven')
+    console.log('Bon appitito')
+};
+dPizza ();
